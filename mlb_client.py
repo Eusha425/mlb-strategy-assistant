9efb5d909d209513becdfef6962e3db88d31c389
Gazi Eusha <52084779+Eusha425@users.noreply.github.com>
2025-02-02 21:40:10 +1100

    add the mlb package

---
+++ mlb_client.py
@@ -0,0 +1,27 @@
+import requests 
+import date
+
+today = date.get_current_date()
+
+# print(today)
+#today = "2024-10-13" # a testing date
+
+# retrieve the request from mlb api in json format
+response = requests.get(f"https://statsapi.mlb.com/api/v1/schedule?sportId=1&date={today}").json()
+#print(req)
+
+#print(req["totalGames"])
+
+#print(req["dates"][0]['games'][0]['gamePk'])
+
+# check if there are any games for the current date
+if response["totalGames"] == 0:
+    print("No game")
+    
+else:
+    #print(req["games"])
+
+    # get the game primary key from the json response
+    gameId = response["dates"][0]['games'][0]['gamePk']
+
+    print(gameId)
