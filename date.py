d697d3d6b80004edfcb437949f82e37b666c6e2b
Gazi Eusha <52084779+Eusha425@users.noreply.github.com>
2025-02-02 19:05:30 +1100

    add the date package

---
+++ date.py
@@ -0,0 +1,12 @@
+import datetime as dt
+
+def get_current_date():
+
+    # use the date time library to get the current date
+    date = dt.date.today().strftime("%y/%m/%d")
+
+    # print(date)
+    return date
+
+if __name__ == "__main__":
+    get_current_date()
