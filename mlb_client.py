e9e28bf3533e8f15fb6ed8d317ef13d78b792981
Gazi Eusha <52084779+Eusha425@users.noreply.github.com>
2025-02-02 21:41:06 +1100

    add game id function

--- mlb_client.py
+++ mlb_client.py
@@ -3,25 +3,28 @@ import date
 
 today = date.get_current_date()
 
-# print(today)
-#today = "2024-10-13" # a testing date
+def get_game_id():
 
-# retrieve the request from mlb api in json format
-response = requests.get(f"https://statsapi.mlb.com/api/v1/schedule?sportId=1&date={today}").json()
-#print(req)
+    # print(today)
+    #today = "2024-10-13" # a testing date
 
-#print(req["totalGames"])
+    # retrieve the request from mlb api in json format
+    response = requests.get(f"https://statsapi.mlb.com/api/v1/schedule?sportId=1&date={today}").json()
+    #print(req)
 
-#print(req["dates"][0]['games'][0]['gamePk'])
+    #print(req["totalGames"])
 
-# check if there are any games for the current date
-if response["totalGames"] == 0:
-    print("No game")
-    
-else:
-    #print(req["games"])
+    #print(req["dates"][0]['games'][0]['gamePk'])
 
-    # get the game primary key from the json response
-    gameId = response["dates"][0]['games'][0]['gamePk']
+    # check if there are any games for the current date
+    if response["totalGames"] == 0:
+        print("No game")
+        
+    else:
+        #print(req["games"])
 
-    print(gameId)
+        # get the game primary key from the json response
+        gameId = response["dates"][0]['games'][0]['gamePk']
+
+        return gameId
+        #print(gameId)
